e398e37362bd925d14112d5349af310320b41368
polina <polina.marinich05@gmail.com>
2025-05-15 23:29:29 +0300

    added javadoc to fcfs

--- src/FCFS.java
+++ src/FCFS.java
@@ -1,11 +1,22 @@
+/**
+ * Implements the First-Come First-Served (FCFS) scheduling algorithm
+ */
 import java.util.*;
 public class FCFS implements Algorithm {
     private ArrayList<Task> queue;
 
+     /**
+     * Creates a new FCFS scheduler with the given list of tasks.
+     * @param tasks the list of tasks to schedule
+     */
     public FCFS(ArrayList<Task> tasks) {
         this.queue = new ArrayList<>(tasks);
     }
 
+     /**
+     * Schedules the tasks in FCFS order.
+     */
+    @Override
     public void schedule() {
         while (!queue.isEmpty()) {
             Task next = pickNextTask();
@@ -13,6 +24,11 @@ public class FCFS implements Algorithm {
         }
     }
 
+     /**
+     * Picks the next task in FCFS order.
+     * @return the next task to run
+     */
+    @Override
     public Task pickNextTask() {
         if(!queue.isEmpty()) return queue.remove(0); // return the first task if not empty
         else return null;
