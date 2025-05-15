7215ca65a1bb858cc8e02df0749ffd26ceb7384c
polina <polina.marinich05@gmail.com>
2025-05-15 23:37:10 +0300

    javadoc for task class

--- src/Task.java
+++ src/Task.java
@@ -1,3 +1,6 @@
+/**
+ * Represents a Task to be scheduled by an algorithm.
+ */
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Task {
@@ -7,6 +10,13 @@ public class Task {
     private static final AtomicInteger next = new AtomicInteger();
     private int tid;
     
+      
+    /**
+     * Creates a new Task with the given name, priority, and CPU burst time.
+     * @param name the name of the task
+     * @param priority the priority of the task (1 to 10)
+     * @param burst the CPU burst time in milliseconds
+     */
     public Task(String name, int priority, int burst) {
         this.tid = next.getAndIncrement();
         this.name = name;
@@ -14,26 +24,53 @@ public class Task {
         this.burst = burst;
     }
 
+     /**
+     * Returns the name of the task.
+     * @return the task name
+     */
     public String getName() {
         return name;
     }
 
+    /**
+     * Returns the priority of the task.
+     * @return the task priority
+     */
     public int getPriority() {
         return priority;
     }
 
+     /**
+     * Sets the burst time of the task.
+     * @param burst the new burst time
+     */
     public void setBurst(int burst) {
         this.burst = burst;
     }
 
+     /**
+     * Returns the burst time of the task.
+     * @return the CPU burst time
+     */
     public int getBurst() {
         return burst;
     }
 
+    /**
+    * Returns a string representation of the task.
+    * Format: [TaskName] PriorityX, Burst: Y
+    *
+    * @return a string describing the task
+    */
     public String toString() {
         return "[" + name + "] Priority" + priority + ", Burst: " + burst; 
     }
 
+    /**
+    * Returns the unique task ID.
+    *
+    * @return the task identifier (tid)
+    */
     public int getTid() {
         return tid;
     }
